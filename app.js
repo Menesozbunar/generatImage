d8b13ac79821fa24302ada4863df2062bc4ecbaf
MuhammedEnesOzbunar <64784788+Menesozbunar@users.noreply.github.com>
2022-12-28 06:11:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const dotenv=require('dotenv').config();
+const path = require('path');
 const openaiRouter=require('./openaiRouter')
 
 const app = express()
  const port=process.env.PORT  ||3000;
- app.set('view engine', 'ejs');
+ 
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
- app.use(express.static('public'));
+ app.use(express.static(path.join(__dirname,"/public")));
 
+ app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+
+ })
 
  app.use('/',openaiRouter)
 
